ecfca19848cb96e1b94382e953a1f57044f8e9f1
murtaz69 <murtaza.ali@stud.sundsgarden.se>
2020-12-18 12:38:56 +0100

    final commit

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -2,18 +2,20 @@ import React, { Component } from "react";
 
 import "./Pokecard.css";
 
-const POKE_APE =
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
 
 class Pokecard extends Component {
   render() {
-    let imageSrc = `${POKE_APE}${this.props.id}.png`;
+    const threeDigit = `00${this.props.id}`.slice(-3);
+    let imageSrc = `${POKE_API}${threeDigit}.png`;
     return (
       <div className="Pokecard">
-        <h1>{this.props.name}</h1>
-        <img src={imageSrc} alt="pokemon" />
-        <div>Type: {this.props.type}</div>
-        <div>EXP: {this.props.exp}</div>
+        <h1 className="Pokecard-title">{this.props.name}</h1>
+        <div className="pokecard-image">
+          <img src={imageSrc} alt="pokemon" />
+        </div>
+        <div className="Pokecard-data">Type: {this.props.type}</div>
+        <div className="Pokecard-data">EXP: {this.props.exp}</div>
       </div>
     );
   }
